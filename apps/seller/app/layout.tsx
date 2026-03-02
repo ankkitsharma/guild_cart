7422bf02e7025ec0903a4ad6eb29b8d4ec302821
ankkitsharma <ankitsharma2801@gmail.com>
2026-03-02 18:51:40 +0530

    Feat: Added Vintage paper system design theme

--- apps/seller/app/layout.tsx
+++ apps/seller/app/layout.tsx
@@ -1,30 +1,36 @@
-import { Geist, Geist_Mono } from "next/font/google"
+import { Libre_Baskerville, Lora, IBM_Plex_Mono } from "next/font/google";
 
-import "@workspace/ui/globals.css"
-import { Providers } from "@/components/providers"
+import "@workspace/ui/globals.css";
+import { Providers } from "@/components/providers";
 
-const fontSans = Geist({
+const fontSans = Libre_Baskerville({
   subsets: ["latin"],
   variable: "--font-sans",
-})
+});
 
-const fontMono = Geist_Mono({
+const fontSerif = Lora({
+  subsets: ["latin"],
+  variable: "--font-serif",
+});
+
+const fontMono = IBM_Plex_Mono({
   subsets: ["latin"],
   variable: "--font-mono",
-})
+  weight: "100",
+});
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body
-        className={`${fontSans.variable} ${fontMono.variable} font-sans antialiased `}
+        className={`${fontSans.variable} ${fontSerif.variable} ${fontMono.variable} antialiased`}
       >
         <Providers>{children}</Providers>
       </body>
     </html>
-  )
+  );
 }
